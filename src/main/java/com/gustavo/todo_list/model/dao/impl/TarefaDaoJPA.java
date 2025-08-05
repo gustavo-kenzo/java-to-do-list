5e8a9a8cedf1cb2e28e2670be1ea13d30a4369a4
Gustavo <sgkenzo212007@gmail.com>
2025-08-05 17:42:48 -0300

    Implementado métodos insert, update e findAll em TarefaDaoJPA

--- src/main/java/com/gustavo/todo_list/model/dao/impl/TarefaDaoJPA.java
+++ src/main/java/com/gustavo/todo_list/model/dao/impl/TarefaDaoJPA.java
@@ -1,7 +1,9 @@
 package com.gustavo.todo_list.model.dao.impl;
 
+import com.gustavo.todo_list.db.DbException;
 import com.gustavo.todo_list.model.dao.TarefaDao;
 import com.gustavo.todo_list.model.entities.Tarefa;
+import com.gustavo.todo_list.util.JPAUtil;
 import jakarta.persistence.EntityManager;
 
 import java.util.List;
@@ -15,11 +17,36 @@ public class TarefaDaoJPA implements TarefaDao {
 
     @Override
     public void insert(Tarefa obj) {
-
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(obj);
+            em.getTransaction().commit();
+        } catch (RuntimeException e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw new DbException("Erro ao inserir nova tarefa " + e.getMessage());
+        } finally {
+            em.close();
+        }
     }
 
     @Override
     public void update(Tarefa obj) {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.merge(obj);
+            em.getTransaction().commit();
+        } catch (RuntimeException e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw new DbException("Erro ao atualizar nova tarefa " + e.getMessage());
+        } finally {
+            em.close();
+        }
 
     }
 
@@ -30,7 +57,14 @@ public class TarefaDaoJPA implements TarefaDao {
 
     @Override
     public List<Tarefa> findAll() {
-        return List.of();
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            return em.createQuery("SELECT t FROM Tarefa t ORDER BY t.createdAt", Tarefa.class).getResultList();
+        } catch (RuntimeException e) {
+            throw new DbException("Erro ao buscar tarefas " + e.getMessage());
+        } finally {
+            em.close();
+        }
     }
 
     @Override
